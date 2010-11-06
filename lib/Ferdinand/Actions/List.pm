630b2c1ad04a95ea63c9739a149b86b45dae98fc
Pedro Melo <melo@simplicidade.org>
2010-11-06 16:53:57 +0000

    Moved setup() and fields from List to parent Action
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Ferdinand/Actions/List.pm
+++ lib/Ferdinand/Actions/List.pm
@@ -8,53 +8,6 @@ use namespace::clean -except => 'meta';
 
 extends 'Ferdinand::Action';
 
-has 'title' => ( isa => 'Str', is  => 'ro' );
-
-has 'column_names' => (
-  isa      => 'ArrayRef',
-  is       => 'ro',
-  required => 1,
-);
-
-has 'columns' => (
-  isa      => 'HashRef',
-  is       => 'ro',
-  required => 1,
-);
-
-
-method setup ($class:, $impl, $meta) {
-  my %clean;
-
-  ## Remove known attributes
-  for my $f (qw( title )) {
-    $clean{$f} = delete $meta->{$f} if exists $meta->{$f};
-  }
-
-  ## Columns
-  my $col_spec = delete($meta->{columns}) || [];
-  confess "Action 'List' requires a 'columns' specification, "
-    unless @$col_spec;
-
-  my @col_order;
-  my %col_meta;
-  while (@$col_spec) {
-    my $name = shift @$col_spec;
-    my $info = ref($col_spec->[0]) eq 'HASH' ? shift @$col_spec : {};
-
-    push @col_order, $name;
-    $col_meta{$name} = $info;
-    
-    $impl->column_meta_fixup($name, $info);
-  }
-
-  return $class->new(
-    %clean,
-    column_names => \@col_order,
-    columns      => \%col_meta,
-  );
-}
-
 
 method render ($impl, $ctx) {
   my $rows = $impl->fetch_rows($self, $ctx);
