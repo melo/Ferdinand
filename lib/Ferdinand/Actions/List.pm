0ee78c27dd85253ddbb37d5789bd786e8add6bdb
Pedro Melo <melo@simplicidade.org>
2010-11-06 00:10:07 +0000

    Added template and render code to the List action
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Ferdinand/Actions/List.pm
+++ lib/Ferdinand/Actions/List.pm
@@ -3,6 +3,7 @@ package Ferdinand::Actions::List;
 
 use Ferdinand::Moose;
 use Method::Signatures;
+use Ferdinand::Utils 'render_template';
 use namespace::clean -except => 'meta';
 
 extends 'Ferdinand::Action';
@@ -54,9 +55,56 @@ method setup ($class:, $meta) {
 
 
 method render ($impl, $ctx) {
-  return 'Listed!'
+  my $rows = $impl->fetch_rows($self, $ctx);
+
+  return render_template(
+    'list.pltj',
+    { action    => $self,
+      col_names => $self->column_names,
+      cols      => $self->columns,
+      rows      => $rows,
+      ctx       => $ctx,
+    }
+  );
 }
 
 
 __PACKAGE__->meta->make_immutable;
 1;
+
+__DATA__
+
+@@ list.pltj
+<?pl #@ARGS cols, col_names, rows, ctx ?>
+
+<table cellspacing="1" class="ordenada1">
+  <thead>
+    <tr>
+<?pl for my $col (@$col_names) { ?>
+      <th>[= $col =]</th>
+<?pl } ?>
+    </tr>
+  </thead>
+  <tbody>
+<?pl if (@$rows) { ?>
+<?pl   for my $row (@$rows) { ?>
+    <tr>
+<?pl     for my $col (@$col_names) { my $ci = $cols->{$col}; ?>
+<?pl       if (my $l = $ci->{linked}) { ?>
+      <td><a href="[= $ctx->{uri_helper}->($l, $row->{_id}) =]">[= $row->{$col} =]</a></td>
+<?pl       } ?>
+<?pl       elsif (my $l = $ci->{link_to}) { ?>
+      <td><a href="[= $l->($row, $ctx) =]">[= $row->{$col} =]</a></td>
+<?pl       } ?>
+<?pl       else { ?>
+      <td>[= $row->{$col} =]</td>
+<?pl       } ?>
+<?pl     } ?>
+    </tr>
+<?pl   } ?>
+<?pl } ?>
+<?pl else { my $n_cols = @$cols; ?>
+    <tr colspan="[= $n_cols =]">Não existem registos para listar</tr>
+<?pl } ?>
+  </tbody>
+</table>
