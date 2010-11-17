848ad93bd43762317156369c5c2a81edc6564f0f
Pedro Melo <melo@simplicidade.org>
2010-11-17 16:05:29 +0000

    Make $sys argument optional on calls to setup()
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Ferdinand/Roles/Setup.pm
+++ lib/Ferdinand/Roles/Setup.pm
@@ -3,14 +3,14 @@ package Ferdinand::Roles::Setup;
 use Ferdinand::Setup 'role';
 use Method::Signatures;
 
-method setup ($class:, $meta, $sys) {
+method setup ($class:, $meta, $sys?) {
   my %attrs;
   $class->setup_attrs(\%attrs, $meta, $sys);
 
   return $class->new(%attrs);
 }
 
-method setup_attrs ($class:, $attrs, $meta, $sys) {}
+method setup_attrs ($class:, $attrs, $meta, $sys?) {}
 
 
 1;
