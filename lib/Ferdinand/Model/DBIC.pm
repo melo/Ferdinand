9473be318cf47bfbcf51ae9782419c5aa3a7eb59
Pedro Melo <melo@simplicidade.org>
2010-11-18 16:10:21 +0000

    Added Ferdinand::DBIC::Model
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ lib/Ferdinand/Model/DBIC.pm
@@ -0,0 +1,54 @@
+package Ferdinand::Model::DBIC;
+# ABSTRACT: Ferdinand model for DBIx::Class sources
+
+use Ferdinand::Setup 'class';
+use Method::Signatures;
+extends 'Ferdinand::Model';
+
+has 'source' => (
+  is       => 'ro',
+  isa      => 'DBIx::Class::ResultSource',
+  required => 1,
+);
+
+
+method column_meta_fixup ($name, $defs) {
+  my %info = %$defs;
+  my $source = $self->source;
+  return \%info unless $source->has_column($name);
+
+  my $ci = $source->column_info($name);
+
+  $info{formatter} = $ci->{extra}{formatter}
+    if exists $ci->{extra}{formatter};
+
+  my $label = $name;
+  if (exists $ci->{extra}{label}) {
+    $label = $ci->{extra}{label};
+  }
+  else {
+    $label = $name;
+    $label =~ s/_/ /g;
+    $label =~ s/\b(\w)/uc($1)/ge;
+    $label =~ s/\bId/ID/g;
+  }
+  $info{label} = $label;
+
+  my $classes = $ci->{extra}{classes} || {};
+  for my $t (qw(list)) {
+    my $cls = $classes->{$t} || [];
+    $cls = [$cls] unless ref $cls;
+    $info{"cls_$t"} = $cls;
+    $info{"cls_${t}_html"} = @$cls? ' class="' . join(' ', @$cls) . '"' : '';
+  }
+}
+
+
+__PACKAGE__->meta->make_immutable;
+1;
+
+=head1 DESCRIPTION
+
+The L<Ferdinand::Impl::DBIC> class is a L<Ferdinand> implementation that
+extracts all the required metadata from a given
+L<DBIx::Class::ResultSource>.
