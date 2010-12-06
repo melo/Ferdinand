68c87d36a772a7d9f63d1ce2894df2cdc2641686
Pedro Melo <melo@simplicidade.org>
2010-12-06 16:46:54 +0000

    Added create mode to Text widget
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Ferdinand/Widgets/Text.pm
+++ lib/Ferdinand/Widgets/Text.pm
@@ -8,20 +8,23 @@ use Ferdinand::Utils qw( render_template );
 use Text::Markdown ();
 
 extends 'Ferdinand::Widget';
-with 'Ferdinand::Roles::ColumnSet';
+with 'Ferdinand::Roles::ColumnSet', 'Ferdinand::Roles::RenderPerMode';
 
-method render_self ($ctx) {
+method render_self_read ($ctx) {
   confess('Record widget requires a valid item() in Context,') unless $ctx->item;
-  $ctx->buffer(render_template('text.pltj', {ctx => $ctx, mkdn => Text::Markdown->new}));
+  $ctx->buffer(render_template('view.pltj', {ctx => $ctx, mkdn => Text::Markdown->new}));
 }
 
+method render_self_write ($ctx) {
+  $ctx->buffer(render_template('create.pltj', {ctx => $ctx}));
+}
 
 __PACKAGE__->meta->make_immutable;
 1;
 
 __DATA__
 
-@@ text.pltj
+@@ view.pltj
 <?pl #@ARGS ctx, mkdn ?>
 <?pl my $widget = $ctx->widget; ?>
 <?pl my $meta = $widget->col_meta; ?>
@@ -36,4 +39,24 @@ __DATA__
 <div>
   [== $mkdn->markdown($v) =]
 </div>
+<?pl } ?>
+
+
+@@ create.pltj
+<?pl #@ARGS ctx ?>
+<?pl my $widget = $ctx->widget; ?>
+<?pl my $meta = $widget->col_meta; ?>
+<?pl my $cols = $widget->col_names; ?>
+<?pl my $params = $ctx->params; ?>
+
+<?pl for my $col (@$cols) {
+        my $ci = $meta->{$col};
+        my $html = $ctx->render_field(
+          field => $col,
+          meta  => $ci,
+          item  => $params,
+        );
+?>
+<h1>[= $ci->{label} =]</h1>
+<div>[== $html =]</div>
 <?pl } ?>
