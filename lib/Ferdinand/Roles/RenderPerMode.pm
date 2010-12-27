44cccdf0cbe910e314ba2bec234341b82de2064b
Pedro Melo <melo@simplicidade.org>
2010-12-27 12:34:24 +0000

    Make sure each mode we stop matching modes after we found the one we want
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/Ferdinand/Roles/RenderPerMode.pm
+++ lib/Ferdinand/Roles/RenderPerMode.pm
@@ -6,8 +6,8 @@ after render_self => sub {
   my $self = shift;
 
   my $m = $_[0]->mode;
-  $self->render_self_read(@_) if $m eq 'view' || $m eq 'list';
-  $self->render_self_write(@_) if $m eq 'create' || $m eq 'create_do';
+  return $self->render_self_read(@_)  if $m eq 'view'   || $m eq 'list';
+  return $self->render_self_write(@_) if $m eq 'create' || $m eq 'create_do';
 };
 
 sub render_self_read  { }
